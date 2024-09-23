6fd47409c32468c3ddc71e5132d577468b21726a
Shubham Mankar <mankarshubham81@gmail.com>
2024-09-23 15:38:23 +0530

    marging for deskstop screen

--- src/components/Responsive.jsx
+++ src/components/Responsive.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Responsive = () => {
   return (
     <div>
-        <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3 m-4">
+        <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3 m-4 md:m-14">
             <div className="shrink-0 h-32 rounded-md bg-red-400" />
             <div className="shrink-0 h-32 rounded-md bg-green-400" />
             <div className="shrink-0 h-32 rounded-md bg-amber-400" />
